8f5fb2ad6e5b6a8f2da73bc25a6093783f403bab
nexustix <nexus1768@tuta.io>
2016-08-07 17:16:58 +0200

    Add implement creation and management of Molecules

---
+++ nxfusion.go
@@ -0,0 +1,88 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/user"
+	"path"
+
+	bp "github.com/nexustix/boilerplate"
+	nrc "github.com/nexustix/nxReplicatorCommon"
+)
+
+// nxfusion add amazingpack supermod
+
+func main() {
+	//fmt.Println("Hello World")
+	args := os.Args
+
+	usr, err := user.Current()
+	bp.FailError(err)
+	workingDir := usr.HomeDir
+	atomDir := nrc.InitWorkFolder(workingDir, ".nxreplicator", "atoms")
+	moleculeDir := nrc.InitWorkFolder(workingDir, ".nxreplicator", "molecules")
+
+	atomManager := nrc.AtomManager{WorkingDir: atomDir}
+	//molecule := nrc.Molecule{}
+
+	action := bp.StringAtIndex(1, args)
+	packName := bp.StringAtIndex(2, args)
+	providerID := bp.StringAtIndex(3, args)
+	atomID := bp.StringAtIndex(4, args)
+
+	fmt.Printf("%s | %s | %s | %s\n", action, packName, providerID, atomID)
+
+	switch action {
+	case "add":
+		addAtom(&atomManager, moleculeDir, packName, providerID, atomID)
+	case "remove":
+		removeAtom(moleculeDir, packName, providerID, atomID)
+	case "list":
+		listAtoms(moleculeDir, packName)
+	}
+}
+
+//addAtom adds atom to molecule
+func addAtom(atomManager *nrc.AtomManager, moleculeDir, packName, providerID, atomID string) {
+	filePath := path.Join(moleculeDir, packName+".nxrm")
+	if atomManager.HasEntry(providerID, atomID) {
+		tmpAtom := atomManager.GetEntry(providerID, atomID)
+
+		tmpMolecule := nrc.Molecule{}
+		tmpMolecule.LoadFromFile(filePath)
+
+		tmpMoleculeItem := nrc.MoleculeItem{Explicit: true, ProviderID: tmpAtom.Provider, AtomID: tmpAtom.ID, Dir: tmpAtom.RelativePath}
+
+		tmpMolecule.AddItem(tmpMoleculeItem)
+		tmpMolecule.SaveToFile(filePath)
+	} else {
+		fmt.Printf("<!> ERROR Atom >%s< not found\n", atomID)
+	}
+}
+
+//removeAtom removes atom from molecule
+func removeAtom(moleculeDir, packName, providerID, atomID string) {
+	filePath := path.Join(moleculeDir, packName+".nxrm")
+
+	tmpMolecule := nrc.Molecule{}
+	tmpMolecule.LoadFromFile(filePath)
+
+	tmpMolecule.RemoveItem(providerID, atomID)
+
+	tmpMolecule.SaveToFile(filePath)
+}
+
+//listAtoms list atoms of molecule
+func listAtoms(moleculeDir, packName string) {
+	filePath := path.Join(moleculeDir, packName+".nxrm")
+
+	tmpMolecule := nrc.Molecule{}
+	tmpMolecule.LoadFromFile(filePath)
+	fmt.Printf("-----\n")
+	for k, v := range tmpMolecule.MoleculeItems {
+		fmt.Printf("(%v) <%v> %s\n", k, v.Explicit, v.AtomID)
+	}
+	fmt.Printf("-----\n")
+}
+
+//func getAtom(){}
